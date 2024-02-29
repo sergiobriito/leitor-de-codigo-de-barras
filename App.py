08d31892f3212b94ef4149ddb640bed5af83a270
Sérgio Brito <64617586+sergiobriito@users.noreply.github.com>
2024-02-29 16:27:30 -0300

    Update App.py

--- App.py
+++ App.py
@@ -26,13 +26,6 @@ if st.button('Executar'):
     ler_arquivos.main()
     st.success('Concluído!', icon="✅")
 
-    filelist=[]
-    for root, dirs, files in os.walk("./"):
-          for file in files:
-                 filename=os.path.join(root, file)
-                 filelist.append(filename)
-    st.write(filelist)
-
     with open("./codigos_de_barras.xlsm","rb") as planilha:
         btDownload = st.download_button(
             label ="📥 Download",
