52f00347ed7c0d6a37a2440d99e56728041e03f2
Sérgio Brito <sergiowilson1999@gmail.com>
2024-02-29 15:51:30 -0300

    build

---
+++ App.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import os
+import glob
+import ler_arquivos
+
+st.set_page_config(page_icon="📄", page_title="Leitor de código de barras")
+st.title("📄 Leitor de código de barras")
+
+uploaded_files = st.file_uploader('Inserir os arquivos:', accept_multiple_files=True)
+
+if os.path.exists("codigos_de_barras.xlsm"):
+    os.remove("codigos_de_barras.xlsm")
+
+arqs = glob.glob('./Arquivos/*')
+for arq in arqs:
+    os.remove(arq)
+
+if st.button('Executar'):
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            file_path = os.path.join("Arquivos", uploaded_file.name)
+            with open(file_path, "wb") as f:
+                f.write(uploaded_file.getvalue())
+                
+    st.success("Processando...")
+    ler_arquivos.main()
+    st.success('Concluído!', icon="✅")
+
+    with open("./codigos_de_barras.xlsm","rb") as planilha:
+        btDownload = st.download_button(
+            label ="📥 Download",
+            data = planilha,
+            file_name="codigos_de_barras.xlsm"
+        )
+
+        if btDownload:
+            st.experimental_rerun()
+
+style = """
+<style>
+#MainMenu {visibility: hidden;}
+header {visibility: hidden;}
+footer {visibility: hidden;}
+.css-12oz5g7 {padding: 2rem 1rem;}
+.css-14xtw13 {visibility: hidden;}
+span.css-9ycgxx.exg6vvm12 {
+visibility: hidden;
+white-space: nowrap;
+}
+span.css-9ycgxx.exg6vvm12::before {
+    visibility: visible;
+    content: "Selecionar os arquivos, ou arraste e solte os arquivos aqui";
+    font-size: 1rem;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 400;
+    line-height: 1.6;
+    text-size-adjust: 100%;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-font-smoothing: auto;
+    color: rgb(49, 51, 63);
+    box-sizing: border-box;
+    margin-bottom: 0.25rem;
+}
+
+small.css-1aehpvj.euu6i2w0 {visibility: hidden;}
+small.css-1aehpvj.euu6i2w0::before {
+    visibility: visible;
+    content: "200MB por arquivo";
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 400;
+    text-size-adjust: 100%;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-font-smoothing: auto;
+    box-sizing: border-box;
+    color: rgba(49, 51, 63, 0.6);
+    font-size: 14px;
+    line-height: 1.25;
+}
+
+section.css-po3vlj.exg6vvm15 button{visibility:hidden;}
+
+#Linkedin {margin-top: 175px;}
+#desenvolvidoPor {color: black;}
+#nome {color: black;}
+</style>
+
+<div id="Linkedin" class="badge-base LI-profile-badge" data-locale="pt_BR" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="sérgio--brito" data-version="v1">
+<a href="https://br.linkedin.com/in/s%C3%A9rgio--brito?trk=profile-badge"><img src="https://brand.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="Linkedin" style="width:42px;height:42px;"></a>
+<a id="desenvolvidoPor">Desenvolvido por </a>
+<a id="nome" class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/s%C3%A9rgio--brito?trk=profile-badge">Sérgio Brito</a>
+</div>
+
+"""
+
+st.markdown(style, unsafe_allow_html=True)
---
+++ ler_arquivos.py
@@ -0,0 +1,71 @@
+from pyzbar import pyzbar
+from openpyxl import load_workbook
+import cv2
+import time
+import os
+from glob import glob
+from pdf2image import convert_from_path
+import streamlit as st
+
+local = os.getcwd()
+caminhoPlanilha = os.path.join(local, "codigos_de_barras_og.xlsm")
+arquivo_excel = load_workbook(caminhoPlanilha, read_only=False, keep_vba=True)
+planilha = arquivo_excel['CÓDIGOS']
+
+# Função para converter PDF em PNG
+def converterPdf():
+    poppler_path = os.path.join(local, "Poppler", "bin")
+    arquivos = glob(os.path.join(local, "Arquivos", "*.pdf"))
+    for arquivo in arquivos:
+        try:
+            print(arquivo)
+            convertido = convert_from_path(arquivo, poppler_path=poppler_path, first_page=1, last_page=1)
+            for i, image in enumerate(convertido):
+                nome = str(arquivo).replace(".pdf", "") + ".png"
+                st.write(nome)
+                image.save(nome, "PNG")
+        except Exception as e:
+            st.write(str(arquivo) + " - ERRO:", e)
+
+# Função para detectar código em imagem(PNG).
+def detectar(imagem,i):
+    zoom = 1.0
+    zoom_step = 0.1
+    while zoom <= 2.0:
+        h, w = imagem.shape[:2]
+        zoomed = cv2.resize(imagem, (int(w*zoom), int(h*zoom)))
+        codigos = pyzbar.decode(zoomed)
+        for codigo in codigos:
+            if codigo.type == "I25" and len(str(codigo.data)) == 47:
+                st.write("Tipo:", codigo.type)
+                st.write("Código:", codigo.data)
+                planilha.cell(row=2+i, column=3, value=codigo.data)
+                planilha.cell(row=2+i, column=2, value=codigo.type)
+                return codigo.data
+        zoom += zoom_step
+        time.sleep(0.1)
+    st.write("Não encontrado.")
+    return None
+
+def main():
+    converterPdf()
+
+    pastaArquivos = os.path.join(local, "Arquivos")
+    arquivos = glob(os.path.join(local, "Arquivos", "*.png"))
+    i = 0
+    for arquivo in arquivos:
+        nome = os.path.basename(arquivo).replace(".png", "")
+        st.write("Nome:", nome)
+        planilha.cell(row=2+i, column=1, value=nome)
+        try:
+            imgBoleto = cv2.imread(arquivo)
+            imgBoleto = detectar(imgBoleto,i)
+        except Exception as e:
+            planilha.cell(row=2+i, column=3, value="Erro na leitura do PDF")
+            st.write("Erro na leitura do PDF:", e)
+
+        arquivo_excel.save("codigos_de_barras.xlsm")
+        i += 1
+
+if __name__ == "__main__":
+    main()
